a201c5151e1d181d0f6ca7eaca86c22755545af1
Damien Siri <dsiri12@gmail.com>
2026-05-07 20:06:34 -0400

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/HomePage';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import './App.css'
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import "./App.css";
 
-function App() {
+export const App = () => {
   return (
     <Routes>
       <Route index path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
     </Routes>
-  )
-}
-
-export default App
+  );
+};
