1f7b9f0e846b2ddf0871ee776da01e4393c5e049
Damien Siri <dsiri12@gmail.com>
2026-05-08 14:01:48 -0400

    Use axios to fetch the data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,13 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import './HomePage.css';
 import {products} from '../../starting-code/data/products'
 import { ProductsCard } from '../components/home/ProductsCard';
 
 export const HomePage = () => {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      response.json();
-    }).then((data) => {
-      console.log(data);
+      console.log(response.data);
     });
 
   return ( 
