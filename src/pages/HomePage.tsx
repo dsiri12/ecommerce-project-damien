d4da5cf16d908d68646c7c60039a4644983811a4
Damien Siri <dsiri12@gmail.com>
2026-05-08 20:29:45 -0400

    use async and await to fetch products

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import { Header } from "../components/Header";
 import "./HomePage.css";
@@ -7,22 +8,38 @@ import type { ProductType } from "../types/ProductType";
 
 export const HomePage = () => {
   const [products, setProducts] = useState<ProductType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<any | null>(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
+        setIsLoading(true);
         const response = await axios.get<ProductType[]>(
           "http://localhost:3000/api/products",
         );
 
         setProducts(response.data);
+        
       } catch (error) {
         console.error("Failed to fetch products:", error);
+        setError(error);
+
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchProducts();
   }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{(error as Error).message}</p>;
+  }
 
   return (
     <>
