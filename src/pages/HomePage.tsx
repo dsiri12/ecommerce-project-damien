e7894540de2fe890c5a4dd960a6d6bee30c7ff14
Damien Siri <dsiri12@gmail.com>
2026-05-08 20:01:52 -0400

    use axios fetch products

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,22 @@
 import axios from 'axios';
 import { Header } from '../components/Header';
 import './HomePage.css';
-import {products} from '../../starting-code/data/products'
 import { ProductsCard } from '../components/home/ProductsCard';
+import { useEffect, useState } from 'react';
+import type { ProductType } from '../types/ProductType';
 
 export const HomePage = () => {
-  axios.get('http://localhost:3000/api/products')
-    .then((response) => {
-      console.log(response.data);
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then((response) => {
+        //console.log(response.data);
+
+        setProducts(response.data as ProductType[])
     });
+  }, []);
+  
 
   return ( 
     <>
