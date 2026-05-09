3281266e059a1622987f695c839e0825ec719b0b
Damien Siri <dsiri12@gmail.com>
2026-05-08 20:18:17 -0400

    use async await

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,36 +1,40 @@
-import axios from 'axios';
-import { Header } from '../components/Header';
-import './HomePage.css';
-import { ProductsCard } from '../components/home/ProductsCard';
-import { useEffect, useState } from 'react';
-import type { ProductType } from '../types/ProductType';
+import axios from "axios";
+import { Header } from "../components/Header";
+import "./HomePage.css";
+import { ProductsCard } from "../components/home/ProductsCard";
+import { useEffect, useState } from "react";
+import type { ProductType } from "../types/ProductType";
 
 export const HomePage = () => {
   const [products, setProducts] = useState<ProductType[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products')
-      .then((response) => {
-        //console.log(response.data);
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get<ProductType[]>(
+          "http://localhost:3000/api/products",
+        );
 
-        setProducts(response.data as ProductType[])
-    });
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Failed to fetch products:", error);
+      }
+    };
+
+    fetchProducts();
   }, []);
-  
 
-  return ( 
+  return (
     <>
       <Header />
 
       <div className="home-page">
         <div className="products-grid">
-
-          {products.map((product)=>{
-            return <ProductsCard key={product.id} product={product} />
+          {products.map((product) => {
+            return <ProductsCard key={product.id} product={product} />;
           })}
-
         </div>
       </div>
     </>
   );
-}
+};
